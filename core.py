c03d051bf6f73d386c2631c4402a874f08c83fb3
Samuel Petersen <samuelkpeter@gmail.com>
2025-09-26 20:32:02 -0300

    lint

--- core.py
+++ core.py
@@ -1,3 +1,26 @@
+from io import BytesIO
+import base64
+import obsws_python as obsws
+from datetime import datetime
+import requests
+import traceback
+import pygetwindow as gw
+import mss
+import asyncio
+import websockets
+import json
+import gc
+import easyocr
+import threading
+import numpy as np
+import cv2
+from core import broadcast
+from core import dialog
+import routines
+from typing import Callable, Dict, List, Optional
+from PIL import Image, ImageFile
+import configparser
+import time
 import sys
 import os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -6,34 +29,12 @@ if __name__ == "__main__":
     print("Initializing...")
     from routines import client_name
     try:
-        from build_info import __version__ # type: ignore
-    except Exception: __version__ = "DEV"
+        from build_info import __version__  # type: ignore
+    except Exception:
+        __version__ = "DEV"
     print(f"Welcome to {client_name.upper()} - build: {__version__}")
     from routines import payload
-import time
-import configparser
-from PIL import Image, ImageFile
-ImageFile.LOAD_TRUNCATED_IMAGES=True
-from typing import Callable, Dict, List, Optional
-import routines
-from core import dialog
-from core import broadcast
-import cv2
-import numpy as np
-import threading
-import easyocr
-import gc
-import json
-import websockets
-import asyncio
-import mss
-import pygetwindow as gw
-import traceback
-import requests
-from datetime import datetime
-import obsws_python as obsws
-import base64
-from io import BytesIO
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -46,19 +47,22 @@ obs = None
 base_height = 1080
 base_width = 1920
 
+
 def print_with_time(*args, **kwargs):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(timestamp, "-", *args, **kwargs)
 
+
 def capture_screen(payload):
     global obs
     if capture_mode == 'obs':
-        try: 
-            if not obs: obs = obsws.ReqClient(
-                host=config.get('obs', 'host', fallback='localhost'),
-                port=config.get('obs', 'port', fallback=4455),
-                password=config.get('obs', 'password', fallback='')
-            )
+        try:
+            if not obs:
+                obs = obsws.ReqClient(
+                    host=config.get('obs', 'host', fallback='localhost'),
+                    port=config.get('obs', 'port', fallback=4455),
+                    password=config.get('obs', 'password', fallback='')
+                )
         except Exception:
             print_with_time("Could not connect to OBS. Retrying...")
             payload['state'] = None
@@ -74,7 +78,11 @@ def capture_screen(payload):
                 )
                 prefix = "base64,"
                 idx = response.image_data.find(prefix)
-                img_str = response.image_data[idx + len(prefix):] if idx != -1 else response.image_data
+                img_str = (
+                    response.image_data[idx + len(prefix):]
+                    if idx != -1
+                    else response.image_data
+                )
                 img_data = base64.b64decode(img_str)
                 img = Image.open(BytesIO(img_data))
             except Exception as e:
@@ -87,10 +95,13 @@ def capture_screen(payload):
             windows = gw.getWindowsWithTitle(executable_title)
             if windows:
                 window = windows[0]
-                if capture_attempts > 0: print(f"Found executable {executable_title}")
+                if capture_attempts > 0:
+                    print(f"Found executable {executable_title}")
                 break
             else:
-                if capture_attempts < 1: print(f"Executable {executable_title} not found. Ensure it is running and visible.")
+                if capture_attempts < 1:
+                    print(
+                        f"Executable {executable_title} not found. Ensure it is running and visible.")
                 capture_attempts += 1
                 payload['state'] = None
                 continue
@@ -99,23 +110,23 @@ def capture_screen(payload):
         # Get the window's dimensions
         width = window.right - window.left
         height = window.bottom - window.top
-        
+
         # Calculate target height for 16:9 aspect ratio
-        target_height = int(width * (9/16))
-        
+        target_height = int(width * (9 / 16))
+
         # If current height is larger than target, adjust top to crop from bottom
         if height > target_height:
             adjusted_top = window.bottom - target_height
         else:
             adjusted_top = window.top
-            
+
         bbox = (window.left, adjusted_top, window.right, window.bottom)
 
         with mss.mss() as sct:
             # Capture the screen using the bounding box
             screenshot = sct.grab(bbox)
             img = Image.frombytes("RGB", screenshot.size, screenshot.rgb)
-    
+
     # also return the scale of the image based off base resolution (1080p)
     image_width, image_height = img.size
     scale_x = image_width / base_width
@@ -124,7 +135,8 @@ def capture_screen(payload):
 
 
 def is_within_deviation(pixel, target_color, deviation):
-    return np.all(np.abs(np.array(pixel[:3] if type(pixel) == tuple else [pixel, pixel, pixel]) - np.array(target_color)) <= 255 * deviation)
+    return np.all(np.abs(np.array(pixel[:3] if type(pixel) is tuple else [pixel, pixel, pixel]) - np.array(target_color)) <= 255 * deviation)
+
 
 def find_color_runs_np(row, color, deviation=0.1):
     runs = []
@@ -143,6 +155,7 @@ def find_color_runs_np(row, color, deviation=0.1):
         runs.append((start_x, len(row) - 1))
     return runs
 
+
 def merge_runs_with_margin(runs, margin, width):
     merged = []
     for start, end in runs:
@@ -157,6 +170,7 @@ def merge_runs_with_margin(runs, margin, width):
             else:
                 merged.append((start, end))
     return merged
+
 
 def stitch_text_regions(image_array, y_line, color, margin=10, deviation=0.1):
     if image_array.ndim == 2:
@@ -179,7 +193,8 @@ def stitch_text_regions(image_array, y_line, color, margin=10, deviation=0.1):
         cropped_strips.append(strip)
 
     total_width = sum(strip.shape[1] for strip in cropped_strips)
-    stitched = np.zeros((image_array.shape[0], total_width, image_array.shape[2] if len(image_array.shape) == 3 else image_array.shape[0]), dtype=image_array.dtype)
+    stitched = np.zeros((image_array.shape[0], total_width, image_array.shape[2] if len(
+        image_array.shape) == 3 else image_array.shape[0]), dtype=image_array.dtype)
 
     x_offset = 0
     for strip in cropped_strips:
@@ -188,32 +203,35 @@ def stitch_text_regions(image_array, y_line, color, margin=10, deviation=0.1):
 
     return stitched
 
+
 def resize_template(template, scale_x, scale_y):
     h, w = template.shape[:2]
     return cv2.resize(template, (int(w * scale_x), int(h * scale_y)), interpolation=cv2.INTER_AREA)
 
-def detect_image(img, scale_x, scale_y, template_file:str, region:tuple[int, int, int, int]=None):
+
+def detect_image(img, scale_x, scale_y, template_file: str, region: tuple[int, int, int, int] = None):
     # Crop the specific area
     if region:
         x, y, w, h = region
         img = img.crop((x, y, x + w, y + h))
     img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
-    
+
     # Load the template images
     template = cv2.imread(template_file, cv2.IMREAD_GRAYSCALE)
-    
+
     if template is None:
         raise FileNotFoundError("Template image not found")
-    
+
     template = resize_template(template, scale_x, scale_y)
-    
+
     # Perform template matching
     res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
     del img
     gc.collect()
     return np.max(res)
 
-def get_color_match_in_region(img, region:tuple[int, int, int, int], target_color:tuple|list[tuple], deviation:float=0.15):
+
+def get_color_match_in_region(img, region: tuple[int, int, int, int], target_color: tuple | list[tuple], deviation: float = 0.15):
     x, y, w, h = region
     cropped_area = img.crop((x, y, x + w, y + h))
     width, height = cropped_area.size
@@ -241,6 +259,7 @@ def get_color_match_in_region(img, region:tuple[int, int, int, int], target_colo
     else:
         return list(matches.values())[0] / total_pixels
 
+
 def remove_neighbor_duplicates(input_list):
     if not input_list:
         return []
@@ -251,7 +270,8 @@ def remove_neighbor_duplicates(input_list):
             result.append(item)
     return result
 
-def crop_inner_area(img, region:tuple[int, int]):
+
+def crop_inner_area(img, region: tuple[int, int]):
     x, w = region
     left = img.crop((0, 0, x, img.height))
     right = img.crop((x + w, 0, img.width, img.height))
@@ -264,20 +284,26 @@ def crop_inner_area(img, region:tuple[int, int]):
     new_img.paste(right, (left.width, 0))
     return new_img
 
-def read_text(img, region: tuple[int, int, int, int]=None, colored:bool=False, contrast:int=1, allowlist:str=None, low_text=0.4):
+
+def read_text(img, region: tuple[int, int, int, int] = None, colored: bool = False, contrast: int = 1, allowlist: str = None, low_text=0.4):
     if region:
         x, y, w, h = region
         img = img.crop((x, y, x + w, y + h))
 
-    if not colored: img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
-    else: img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-    if contrast: img = cv2.convertScaleAbs(img, alpha=contrast, beta=-(contrast * 50))
+    if not colored:
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
+    else:
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    if contrast:
+        img = cv2.convertScaleAbs(img, alpha=contrast, beta=-(contrast * 50))
 
-    result = reader.readtext(img, paragraph=False, allowlist=allowlist, low_text=low_text)
+    result = reader.readtext(img, paragraph=False,
+                             allowlist=allowlist, low_text=low_text)
 
     if result:
         result = [res[1] for res in result]
-    else: result = None
+    else:
+        result = None
     if config.getboolean('settings', 'debug_mode', fallback=False):
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         filename = f"dev/{'_'.join(result) if isinstance(result, list) else ''}_{timestamp}.png"
@@ -286,6 +312,7 @@ def read_text(img, region: tuple[int, int, int, int]=None, colored:bool=False, c
     gc.collect()
 
     return result
+
 
 def get_latest_build_number():
     url = f'https://api.github.com/repos/skpeter/{client_name}/releases/latest'
@@ -299,9 +326,11 @@ def get_latest_build_number():
         print(f"Update check failed: {e}")
     return None
 
+
 def is_update_available():
     latest = get_latest_build_number()
     return latest if latest is not None and int(__version__) < latest else False
+
 
 def run_detection_loop(
     state_to_functions: Dict[Optional[str], List[Callable]],
@@ -314,7 +343,8 @@ def run_detection_loop(
             img, scale_x, scale_y = capture_screen(payload)
             functions = state_to_functions.get(payload.get('state'), [])
             for func in functions:
-                if not func: continue
+                if not func:
+                    continue
                 func(payload, img, scale_x, scale_y)
         except Exception as e:
             print(f"Error: {str(e)}")
@@ -324,6 +354,7 @@ def run_detection_loop(
         sleep_time = refresh_rate - elapsed
         if sleep_time > 0:
             time.sleep(sleep_time)
+
 
 async def send_data(payload, websocket):
     try:
@@ -344,17 +375,23 @@ async def send_data(payload, websocket):
         if "no close frame received or sent" not in str(e):
             print(f"Connection error from client: {e}")
 
-async def receive_data(payload:dict, websocket):
+
+async def receive_data(payload: dict, websocket):
     try:
         async for message in websocket:
-            if "confirm-entrants:" in message and processing_message == False and config.get('settings', 'capture_mode') == 'game':
-                print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "- Received request to confirm players:", str(message).replace("confirm-entrants:", "").strip().split(":"))
-                if str(payload['players'][0]['name']) in str(message) and str(payload['players'][1]['name']) in str(message): return True
+            if "confirm-entrants:" in message and processing_message is False and config.get('settings', 'capture_mode') == 'game':
+                print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "- Received request to confirm players:",
+                      str(message).replace("confirm-entrants:", "").strip().split(":"))
+                if str(payload['players'][0]['name']) in str(message) and str(payload['players'][1]['name']) in str(message):
+                    return True
+
                 def doTask():
                     global processing_message
                     processing_message = True
-                    players = str(message).replace("confirm-entrants:", "").strip().split(":")
-                    chosen_player = dialog.choose_player_side(players[0], players[1])
+                    players = str(message).replace(
+                        "confirm-entrants:", "").strip().split(":")
+                    chosen_player = dialog.choose_player_side(
+                        players[0], players[1])
                     if chosen_player == players[0]:
                         payload['players'][0]['name'] = players[0]
                         payload['players'][1]['name'] = players[1]
@@ -370,7 +407,8 @@ async def receive_data(payload:dict, websocket):
         if "no close frame received or sent" not in str(e):
             print(f"Connection error from client: {e}")
 
-async def handle_connection(websocket, payload:dict):
+
+async def handle_connection(websocket, payload: dict):
     send_task = asyncio.create_task(send_data(payload, websocket))
     receive_task = asyncio.create_task(receive_data(payload, websocket))
     done, pending = await asyncio.wait(
@@ -380,11 +418,12 @@ async def handle_connection(websocket, payload:dict):
     for task in pending:
         task.cancel()
 
-def start_websocket_server(payload:dict):
+
+def start_websocket_server(payload: dict):
     import websockets
     import asyncio
 
-    async def start_server(payload:dict):
+    async def start_server(payload: dict):
         async with websockets.serve(
             lambda ws: handle_connection(ws, payload),
             "0.0.0.0",
@@ -396,11 +435,15 @@ def start_websocket_server(payload:dict):
             await asyncio.Future()  # run forever
     asyncio.run(start_server(payload))
 
+
 if __name__ == "__main__":
     new_ver = is_update_available()
-    if new_ver: print(f"New build {new_ver} available (you are on build {__version__}). Head over to \nhttps://github.com/skpeter/{client_name} to download it.")
-    broadcast_thread = threading.Thread(target=broadcast.broadcast_device_info, args=(routines.client_name,), daemon=True).start()
-    websocket_thread = threading.Thread(target=start_websocket_server, args=(payload,), daemon=True).start()
+    if new_ver:
+        print(f"New build {new_ver} available (you are on build {__version__}). Head over to \nhttps://github.com/skpeter/{client_name} to download it.")
+    broadcast_thread = threading.Thread(target=broadcast.broadcast_device_info, args=(
+        routines.client_name,), daemon=True).start()
+    websocket_thread = threading.Thread(
+        target=start_websocket_server, args=(payload,), daemon=True).start()
     print("All systems go. Please head to the character or stage selection screen to start detection.\n")
     run_detection_loop(routines.states_to_functions, payload)
     while True:
